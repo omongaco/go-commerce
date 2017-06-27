ea08601e4c68c658071f498bff83912569e1dea1
Clayton Ray <iamclaytonray@gmail.com>
2017-06-27 18:00:13 -0400

    Delete models/category.go

--- models/category.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gopkg.in/mgo.v2/bson"
-
-// Category models our data for the database
-type Category struct {
-	ID   bson.ObjectId `json:"_id" bson:"_id"`
-	Name string        `json:"name" bson:"name"`
-}
